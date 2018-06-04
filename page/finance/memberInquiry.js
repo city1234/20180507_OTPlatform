cb2dfead181b621067b0d04da8066834756a91bf
amandahong <amanda.hong@timecitytech.com>
2018-06-04 11:20:32 +0800

    commit

--- page/finance/memberInquiry.js
+++ page/finance/memberInquiry.js
@@ -19,17 +19,28 @@ layui.use(['form', 'layer', 'laydate', 'table', 'laytpl'], function() {
         cols: [
             [
                 // {type: "checkbox", fixed:"left", width:50},
-                { field: 'time', title: '新增日期', width: 140 , align: "center" },
+                { field: 'time', title: '新增日期', width: 140, align: "center" },
                 { field: 'distributor', title: '总代理', width: 140, align: "center" },
                 { field: 'parentAgent', title: '上级代理', width: 140, align: 'center' },
-                { field: 'account', title: '账号', align: 'center', templet:function(d){
-                    return '<a class="layui-blue" lay-event="details">'+d.account+'</a>'
-                }},
+                {
+                    field: 'account',
+                    title: '账号',
+                    align: 'center',
+                    templet: function(d) {
+                        return '<a class="layui-blue" lay-event="details">' + d.account + '</a>'
+                    }
+                },
                 { field: 'name', title: '真实姓名', align: 'center' },
-                { field: 'balance', title: '余额', width: 100, align: 'center', templet:function(d){
-                    return '<a class="layui-blue" lay-event="view">'+d.balance+'</a>'
-                }},
-                { field: 'depositTimes', title: '存款次数', align: 'center'},
+                {
+                    field: 'balance',
+                    title: '余额',
+                    width: 100,
+                    align: 'center',
+                    templet: function(d) {
+                        return '<a class="layui-blue" lay-event="view">' + d.balance + '</a>'
+                    }
+                },
+                { field: 'depositTimes', title: '存款次数', align: 'center' },
                 { field: 'totalDeposits', title: '存款总数', width: 140, align: 'center' },
                 { field: 'withdrawals', title: '提款次数', width: 140, align: 'center' },
                 { field: 'manualWithdrawals', title: '人工提款次数', width: 140, align: 'center' },
@@ -38,7 +49,7 @@ layui.use(['form', 'layer', 'laydate', 'table', 'laytpl'], function() {
                 { field: 'E-mail', title: '电子邮箱', width: 140, align: 'center' },
                 { field: 'QQNum', title: 'QQ', width: 140, align: 'center' },
                 { field: 'registerIP', title: '注册IP', width: 140, align: 'center' },
-                { field: 'memberStatus',title: '状态', width: 140, align: 'center', fixed:"right" , templet:"#memberStatusBar"},
+                { field: 'memberStatus', title: '状态', width: 140, align: 'center', fixed: "right", templet: "#memberStatusBar" },
             ]
         ]
     });
@@ -58,7 +69,7 @@ layui.use(['form', 'layer', 'laydate', 'table', 'laytpl'], function() {
             layer.msg("请输入搜索的内容");
         }
     });
-    
+
     //列表操作
     table.on('tool(memberInquiry)', function(obj) {
         var layEvent = obj.event,
@@ -69,33 +80,33 @@ layui.use(['form', 'layer', 'laydate', 'table', 'laytpl'], function() {
                 title: "会员资料",
                 type: 1,
                 btn: '关闭',
-                area:['500px','500px'],
+                area: ['500px', '67%'],
                 content: '<table class="layui-table rebateChargeoff">' +
-                '<tr><th>账号</th><td>34wr</td></tr>'+
-                '<tr><th>名称</th><td> </td></tr>'+
-                '<tr><th>生日日期</th><td> </td></tr>'+
-                '<tr><th>国家</th><td> </td></tr>'+
-                '<tr><th>电话号码</th><td>123132</td></tr>'+
-                '<tr><th>电子邮箱</th><td>123@123.com</td></tr>'+
-                '<tr><th>QQ</th><td>15646</td></tr>'+
-                '<tr><th>币别</th><td>RMB</td></tr>'+
-                '<tr><th>现金金额</th><td>200.0</td></tr>'+
-                '<tr><th>新增日期</th><td>2018-05-31</td></tr>'+
-                '<tr><th>状态</th><td>正常</td></tr>'+
-                '<tr><th>代理商</th><td>sfsf</td></tr>'+
-            '</table>'
+                    '<tr><th>账号</th><td>34wr</td></tr>' +
+                    '<tr><th>名称</th><td> </td></tr>' +
+                    '<tr><th>生日日期</th><td> </td></tr>' +
+                    '<tr><th>国家</th><td> </td></tr>' +
+                    '<tr><th>电话号码</th><td>123132</td></tr>' +
+                    '<tr><th>电子邮箱</th><td>123@123.com</td></tr>' +
+                    '<tr><th>QQ</th><td>15646</td></tr>' +
+                    '<tr><th>币别</th><td>RMB</td></tr>' +
+                    '<tr><th>现金金额</th><td>200.0</td></tr>' +
+                    '<tr><th>新增日期</th><td>2018-05-31</td></tr>' +
+                    '<tr><th>状态</th><td>正常</td></tr>' +
+                    '<tr><th>代理商</th><td>sfsf</td></tr>' +
+                    '</table>'
             })
-        } else if( layEvent === 'view'){
+        } else if (layEvent === 'view') {
             layer.open({
                 title: "查看余额",
                 type: 1,
                 btn: '关闭',
-                area:['1100px','420px'],
+                area: ['1100px', '420px'],
                 content: '<table class="layui-table rebateChargeoff">' +
-                '<tr><th>总金额</th><th>现金余额</th><th>传统彩余额</th><th>皇冠体育余额</th><th>AG余额</th><th>OG余额</th><th>BB余额</th><th>MG余额</th><th>沙巴余额</th><th>OPUS余额</th><th>LEBO余额</th><th>AB余额</th><th>DS余额</th><th>MWG余额</th><th>PT余额</th><th>新MG余额</th><th>PP余额</th><th>CMD余额</th><th>VG余额</th><th>VGS余额</th><th>CQ9余额</th></tr>'+
-                '<tr><td>350</td><td>350</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td></tr>' +
-                '<tr><td colspan="21"><a class="layui-btn layui-btn-normal">更新</a></td></tr>' +
-                '</table>'
+                    '<tr><th>总金额</th><th>现金余额</th><th>传统彩余额</th><th>皇冠体育余额</th><th>AG余额</th><th>OG余额</th><th>BB余额</th><th>MG余额</th><th>沙巴余额</th><th>OPUS余额</th><th>LEBO余额</th><th>AB余额</th><th>DS余额</th><th>MWG余额</th><th>PT余额</th><th>新MG余额</th><th>PP余额</th><th>CMD余额</th><th>VG余额</th><th>VGS余额</th><th>CQ9余额</th></tr>' +
+                    '<tr><td>350</td><td>350</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td></tr>' +
+                    '<tr><td colspan="21"><a class="layui-btn layui-btn-normal">更新</a></td></tr>' +
+                    '</table>'
             })
         }
     });
--- page/quotaConvert/quotaConvertRecord.js
+++ page/quotaConvert/quotaConvertRecord.js
@@ -19,20 +19,31 @@ layui.use(['form', 'layer', 'laydate', 'table', 'laytpl'], function() {
         cols: [
             [
                 // {type: "checkbox", fixed:"left", width:50},
-                { field: 'CR-memberAccount', title: '会员账号', width: 140 , align: "center" },
+                { field: 'CR-memberAccount', title: '会员账号', width: 140, align: "center" },
                 { field: 'CR-intoPlatform', title: '转入平台', width: 140, align: "center" },
                 { field: 'CR-outPlatform', title: '转出平台', width: 140, align: 'center' },
                 { field: 'CR-orderNumber', title: '单号', align: 'center' },
                 { field: 'CR-orderNumberAPI', title: 'API单号', align: 'center' },
                 { field: 'CR-transaction', title: '交易金额', width: 100, align: 'center' },
-                { field: 'CR-time', title: '交易时间', align: 'center'},
-                { field: 'CR-status', title: '状态', width: 140, align: 'center', templet:function(d){
-                    return d.CR-status === "0" ? "未交易" : "已确认";
-                }},
+                { field: 'CR-time', title: '交易时间', align: 'center' },
+                {
+                    field: 'CR-status',
+                    title: '状态',
+                    width: 140,
+                    align: 'center',
+                    templet: function(d) {
+                        return d.CR - status === "0" ? "未交易" : "已确认";
+                    }
+                },
                 { field: 'CR-note', title: '备注', width: 140, align: 'center' },
-                { title: '操作', align: 'center', width: 100,templet: function(d) {
-                    return '<a class="layui-btn layui-btn-border-normal layui-btn-xs" lay-event="operator">操作记录</a>'
-                }}
+                {
+                    title: '操作',
+                    align: 'center',
+                    width: 100,
+                    templet: function(d) {
+                        return '<a href="operatorRecord.html" class="layui-btn layui-btn-border-normal layui-btn-xs">操作记录</a>'
+                    }
+                }
             ]
         ]
     });
@@ -52,7 +63,7 @@ layui.use(['form', 'layer', 'laydate', 'table', 'laytpl'], function() {
             layer.msg("请输入搜索的内容");
         }
     });
-  
+
     //列表操作
     table.on('tool(quotaConvertRecord)', function(obj) {
         var layEvent = obj.event,
@@ -60,17 +71,17 @@ layui.use(['form', 'layer', 'laydate', 'table', 'laytpl'], function() {
 
         if (layEvent === 'operator') { //操作记录
             var index = layui.layer.open({
-                title : "操作记录",
-                type : 2,
-                content : "operatorRecord.html",
-                success : function(layero, index){
+                title: "操作记录",
+                type: 2,
+                content: "operatorRecord.html",
+                success: function(layero, index) {
                     var body = layui.layer.getChildFrame('body', index);
                 }
             })
             layui.layer.full(index);
-            window.sessionStorage.setItem("index",index);
+            window.sessionStorage.setItem("index", index);
             //改变窗口大小时，重置弹窗的宽高，防止超出可视区域（如F12调出debug的操作）
-            $(window).on("resize",function(){
+            $(window).on("resize", function() {
                 layui.layer.full(window.sessionStorage.getItem("index"));
             })
         }
